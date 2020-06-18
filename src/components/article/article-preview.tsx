d5b2186e28da8c8d9932cb3f079cd796fbf6e814
Yoan Gauthier <yoangau@gmail.com>
2020-06-18 13:43:11 -0400

    add fade bottom

--- src/components/article/article-preview.tsx
+++ src/components/article/article-preview.tsx
@@ -4,6 +4,8 @@ import { ArticleProps } from './article.interface';
 import { useSelector } from 'react-redux';
 import { selectLanguage } from '../language/language.slice';
 import { useHistory } from 'react-router-dom';
+// @ts-ignore
+import Fade from 'react-reveal/Fade';
 
 export const ArticlePreview = ({ article }: ArticleProps) => {
   const language = useSelector(selectLanguage);
@@ -11,20 +13,22 @@ export const ArticlePreview = ({ article }: ArticleProps) => {
   const [cursor, changeCursor] = useState('');
 
   return (
-    <Card style={{ margin: '20px', maxWidth: '512px' }}>
-      <Card.Img
-        onClick={() => {
-          history.push(article.url);
-        }}
-        onMouseEnter={() => changeCursor('pointer')}
-        style={{ cursor: cursor }}
-        variant="top"
-        src={article.img}
-      />
-      <Card.Body>
-        <Card.Title>{article.title[language]}</Card.Title>
-        <Card.Text>{article.description[language]}</Card.Text>
-      </Card.Body>
-    </Card>
+    <Fade bottom>
+      <Card style={{ margin: '20px', maxWidth: '512px' }}>
+        <Card.Img
+          onClick={() => {
+            history.push(article.url);
+          }}
+          onMouseEnter={() => changeCursor('pointer')}
+          style={{ cursor: cursor }}
+          variant="top"
+          src={article.img}
+        />
+        <Card.Body>
+          <Card.Title>{article.title[language]}</Card.Title>
+          <Card.Text>{article.description[language]}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Fade>
   );
 };
